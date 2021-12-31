83d7246f4d49227b0eac560dec26205cf1fcb038
Ayça Seçer <46239706+aycasecr@users.noreply.github.com>
2021-12-31 17:34:42 +0300

    Add files via upload
    First commit

---
+++ Week1/Week1_Homeworks/Week1_Homeworks/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week1_Homeworks.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string  MovieTitle { get; set; }
+        public string  MovieOverview { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public double VoteAverage { get; set; }
+    }
+}

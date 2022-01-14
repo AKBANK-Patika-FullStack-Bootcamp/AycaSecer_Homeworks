1227c2c506443945d393f45d3a4735647e82f805
Ayça Seçer <46239706+aycasecr@users.noreply.github.com>
2022-01-14 18:53:18 +0300

    Add files via upload

---
+++ Week4/DAL/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public string MovieOverview { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public double VoteAverage { get; set; }
+        public int GenresId { get; set; }
+    }
+}
